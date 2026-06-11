2807beebda4fcfa266ee6e1d89f2a07d45f7ff17
Mateus Guerra <mateus26fotos@gmail.com>
2026-06-11 01:17:35 -0300

    feat:url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: ' https://panel-ascl-pio-sip-1.onrender.com'
 };
 
-//https://panel-ascl-pio-sip.onrender.com
+// Produção ->  https://panel-ascl-pio-sip.onrender.com
 
 ////http://localhost:8080
+
+// banco de dados de Dev -> https://panel-ascl-pio-sip-1.onrender.com
